f302e00cfc261fb31c9d6449ced91b7a8eb6d28c
Rakib Khan <rakib.cse.sust@adpeople.onmicrosoft.com>
2020-08-30 14:38:48 +0600

    firestore ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:service_monitor/service.card.dart';
 import 'service.model.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Dashboard());
 }
 
@@ -30,6 +34,28 @@ class _DashboardState extends State<Dashboard> {
         successCount: 17),
   ];
 
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final title = 'Dashboard';
@@ -42,15 +68,32 @@ class _DashboardState extends State<Dashboard> {
           centerTitle: true,
           title: Text(title),
         ),
-        body: GridView.count(
-          padding: EdgeInsets.all(5.0),
-          crossAxisCount: 2,
-          children: services
-              .map(
-                (service) => ServiceCard(service: service),
-              )
-              .toList(),
-        ),
+        body: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('services').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: Text('Loading ...'),
+                );
+              }
+              if (!snapshot.hasData) {
+                return Center(
+                  child: Text('No data ...'),
+                );
+              }
+              return GridView.builder(
+                  padding: EdgeInsets.all(5.0),
+                  itemCount: snapshot.data.docs.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot ds = snapshot.data.docs[index];
+                    Service service = Service.fromMap(ds.data(), ds.id);
+                    return ServiceCard(service: service);
+                  });
+            }),
       ),
     );
   }
