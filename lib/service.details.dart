0b72b9bf20028c07534e46f5225b56e96a925564
Rakib Khan <rakib.cse.sust@adpeople.onmicrosoft.com>
2020-09-04 21:18:14 +0600

    graph

--- lib/service.details.dart
+++ lib/service.details.dart
@@ -1,7 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:service_monitor/card_message.dart';
 import 'package:service_monitor/service.model.dart';
-
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'firestore.service.dart';
 
 class ServiceDetails extends StatefulWidget {
@@ -15,9 +16,40 @@ class ServiceDetails extends StatefulWidget {
 
 class _ServiceDetailsState extends State<ServiceDetails> {
   FirestoreService _service = FirestoreService();
+  List<charts.Series> seriesList;
+
+  static List<charts.Series<Sales, String>> _createRandomData() {
+    final random = Random();
+    final salesData = [
+      Sales('2', random.nextInt(10)),
+      Sales('4', random.nextInt(10)),
+      Sales('6', random.nextInt(10)),
+      Sales('8', random.nextInt(10)),
+      Sales('10', random.nextInt(10)),
+      Sales('12', random.nextInt(10)),
+      Sales('14', random.nextInt(10)),
+      Sales('16', random.nextInt(10)),
+      Sales('18', random.nextInt(10)),
+      Sales('20', random.nextInt(10)),
+      Sales('22', random.nextInt(10)),
+      Sales('0', random.nextInt(10))
+    ];
+    return [
+      charts.Series<Sales, String>(
+        id: 'Sales',
+        domainFn: (Sales sales, _) => sales.year,
+        measureFn: (Sales sales, _) => sales.sales,
+        data: salesData,
+        colorFn: (datum, index) =>
+            charts.ColorUtil.fromDartColor(Colors.red[800]),
+      )
+    ];
+  }
+
   @override
   void initState() {
     super.initState();
+    seriesList = _createRandomData();
   }
 
   @override
@@ -26,6 +58,33 @@ class _ServiceDetailsState extends State<ServiceDetails> {
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: Text(widget.item.serviceName),
+        actions: [
+          new IconButton(
+              icon: Icon(
+                Icons.insert_chart,
+                color: Colors.white,
+                size: 35,
+              ),
+              onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('7 days failed status graph'),
+                      actions: [
+                        Container(
+                          height: 400,
+                          width: 400,
+                          padding: EdgeInsets.all(5),
+                          child: charts.BarChart(
+                            seriesList,
+                            animate: true,
+                            vertical: true,
+                          ),
+                        )
+                      ],
+                    );
+                  }))
+        ],
       ),
       body: Center(
         child: StreamBuilder<List<ServiceLogDetails>>(
@@ -48,9 +107,8 @@ class _ServiceDetailsState extends State<ServiceDetails> {
                   int cpu = service.cpu == 0
                       ? 0
                       : ((service.cpu * 100) / widget.item.cpu).ceil();
-
                   return Card(
-                    margin: EdgeInsets.all(5),
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
                     elevation: 4,
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
@@ -80,7 +138,7 @@ class _ServiceDetailsState extends State<ServiceDetails> {
                                       style: TextStyle(color: Colors.grey)),
                                   SizedBox(width: 15),
                                   Icon(Icons.sim_card,
-                                      color: Colors.green[500]),
+                                      color: Colors.green[500], size: 20),
                                   SizedBox(width: 2),
                                   Text('$memory %',
                                       style: TextStyle(color: Colors.grey)),
@@ -110,4 +168,11 @@ class _ServiceDetailsState extends State<ServiceDetails> {
       ),
     );
   }
+}
+
+class Sales {
+  final String year;
+  final int sales;
+
+  Sales(this.year, this.sales);
 }
